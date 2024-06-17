f3bfb93a81cc500b90369e72c119201317ca556f
insssu <xmrtn210845@naver.com>
2024-06-17 13:41:58 +0900

    6/17 오전수업

--- javascript/user_management.js
+++ javascript/user_management.js
@@ -3,20 +3,23 @@ console.log(names.join(""));
 
 let userList = [];
 let epmtyUser = {
+    name:"",
     username: "",
     password: ""
 };
 
 let user = {        // 초기화용
-    ...epmtyUser
+    ...epmtyUser    // 새로운 주소안에 username과 password의 값을 넣은 것.
 }
 
 function renderTable() {
-    const userTableBody = document.querySelector(".user-table-body");
-    userTableBody.innerHTML = userList.map(({username, password}, index) => {
+    const userTableBody = document.querySelector(".user-table-body");       // document.() : 문서 전체에서 요소를 가져온다.
+    userTableBody.innerHTML = userList.map(({name, username, password}, index) => {   // 호이스팅은 function 정의에서 일어난다.
         return `
             <tr>
                 <td>${index + 1}</td>
+                <td>${name}</td>
+                // <td>                   안닫아도 되나 ??
                 <td>${username}</td>
                 <td>${password}</td>
             </tr>
@@ -28,28 +31,37 @@ function renderTable() {
 
 function handleUserInputKeyDown(e) {
     user = {
-        ...user,                 // 입력된 값을 자기 자신에게 계속 쌓는 것
+        ...user,                 // 입력된 값을 자기 자신에게 계속 쌓는 것. 객체를 계속 새로 만들어주기 때문에 주소가 바뀜.
         [e.target.name]: e.target.value
     }
+
+    // user[e.target.name] = e.target.value;   // 주소는 고정.
 
     console.log(user);
 
     if(e.keyCode === 13) {       // 엔터가 들어왔을 때,
+        const nameInput = document.querySelector(".name-input");
         const usernameInput = document.querySelector(".username-input");
         const passwordInput = document.querySelector(".password-input");
+
+        if(e.target.name === "name") {
+            usernameInput.focus();
+        }
 
         if(e.target.name === "username") {
             passwordInput.focus();
         }
+
         if(e.target.name === "password") {
             userList = [ ...userList, { ...user } ];        // userList에 새로운 정보 추가. 스프레드를 사용해서 값만 새로운 객체로 변경해 주는것이 좋다.
 
             renderTable();      // 추가 되었으니 새로운 정보를 꾸려줘야 한다.
 
+            nameInput.value = epmtyUser.name;
             usernameInput.value = epmtyUser.username;   // 빈 값으로 바꿔주고
             passwordInput.value = epmtyUser.password;
 
-            usernameInput.focus();      // username으로 다시 포커스를 보내줘서 새로운 값을 받을 수 있도록.
+            nameInput.focus();      // name으로 다시 포커스를 보내줘서 새로운 값을 받을 수 있도록.
         }
     }
     console.log(e.target.name);
